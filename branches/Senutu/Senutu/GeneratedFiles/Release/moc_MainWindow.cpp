5843366c753e3967b7e874f24aae05662b162e78
qiaoyu.yu <qiaoyu.yu@660b08d5-9609-fd5f-b021-df186fb285f7>
2010-06-03 15:40:56 +0000

    请修改：1. 文件时程序死锁         2.加入播放、停止、快进、快退等基本功能，调用CAudioCtrl即可

--- branches/Senutu/Senutu/GeneratedFiles/Release/moc_MainWindow.cpp
+++ branches/Senutu/Senutu/GeneratedFiles/Release/moc_MainWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MainWindow.h'
 **
-** Created: Wed May 5 22:24:28 2010
+** Created: Thu Jun 3 22:54:29 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
