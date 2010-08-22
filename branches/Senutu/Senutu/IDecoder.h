b309d9fc5ac05f81fdc4f5fdcb90f5d1ffce2a68
qiaoyu.yu <qiaoyu.yu@660b08d5-9609-fd5f-b021-df186fb285f7>
2010-08-22 06:46:02 +0000

    * 在IDecoder中增加TAG结构存储歌名、演唱者等信息，实现MP3的读取ID3功能 * Lyrics增加下载功能
    TODO: 千千静听歌词服务器的使用

--- branches/Senutu/Senutu/IDecoder.h
+++ branches/Senutu/Senutu/IDecoder.h
@@ -4,9 +4,22 @@
 
 #include "aerror.h"
 #include "XAudio2Manip.h"
- 
+
+//This struct defines the tag. e.g. ID3, APE tag, WMA tag...
+struct TAG
+{
+	string Title;
+	string Artist;
+	string Album;
+	string Year;
+	string Comment;
+	string Genre;
+};
+typedef struct TAG TAG;
+//----------------------------------------------------------------------------
+
 //-----------------------------------------------------------------------------
-// Name: class CDecoder
+// Name: class IDecoder
 // Desc: the base class of all decoders
 //-----------------------------------------------------------------------------
 class IDecoder{
@@ -18,21 +31,22 @@ public:
     virtual ARESULT Open( LPWSTR strFileName) = 0;
     virtual ARESULT Play();
     virtual ARESULT Pause();
-    
     virtual ARESULT Stop() = 0;//the file is still open, use |close()| to release the file;
     virtual ARESULT Sync(int minBufTime=500,int MaxBufTime=1000,int BufferSpan=DEFAULT_BUFFER_SPAN)=0;
     //time unit is millisecond
-    virtual int GetFullTime() = 0;
+    
+	virtual int GetFullTime() = 0;
+	virtual ARESULT SetCurTime(int time) = 0;
     virtual int GetCurTime() = 0;
+
 	virtual bool isPlaying() {return m_bIsPlaying;}
 	virtual bool isPause() {return m_bIsPause;}
-	
-    virtual ARESULT SetCurTime(int time) = 0;
-    
     virtual ARESULT Close() = 0;//release the file
+
 	virtual float GetVolume();
 	virtual ARESULT SetVolume(float theVolume);
 
+	virtual TAG GetTag(){return m_tag;}
 protected:
     WAVEFORMATEX * m_pwfx;       //pointer to waveformatex structure
     
@@ -43,9 +57,8 @@ protected:
     bool m_bIsPlaying;
     bool m_bIsPause;
     bool m_bIsStop;
-     
-   
 
+	TAG m_tag; //stores the tag info
 };
 
 
--- branches/Senutu/Senutu/Lyrics.cpp
+++ branches/Senutu/Senutu/Lyrics.cpp
@@ -1,9 +1,34 @@
 #include "Lyrics.h"
 
-
-Lyrics::Lyrics(string artist, string title):m_Artist(artist), m_Title(title) {}
-
-bool Lyrics::DownloadLyrics(string artist, string title)
+Lyrics::Lyrics(QString artist, QString title):m_Artist(artist), m_Title(title) 
 {
+	connect(&m_NetworkManager, SIGNAL(finished(QNetworkReply*)),SLOT(OnDownloadFinished(QNetworkReply*)));
+}
 
+//bool Lyrics::DownloadLyrics(string artist, string title)
+//{
+//	return true;
+//}
+
+void Lyrics::DownloadFromUrl(QString url)
+{
+	QUrl URL = QUrl::fromEncoded(url.toLocal8Bit());
+	QNetworkRequest request(URL);
+	QNetworkReply * reply = m_NetworkManager.get(request);
+	m_CurrentDownloads.append(reply);
+}
+
+QString Lyrics::OnDownloadFinished(QNetworkReply * reply)
+{
+ 	if (reply->error()) {
+ 		atrace_error(qPrintable(reply->errorString()),AR_ERROR_WHILE_DOWNLOADING_LYRICS);  //cannot download from this url
+ 		return m_Title;
+	} 
+	else 
+	{
+		QString result(reply->readAll());    //the whole page of the whole file
+		m_CurrentDownloads.removeAll(reply);
+		reply->deleteLater();
+		return result;
+	}
 }
